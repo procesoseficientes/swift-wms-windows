8aba05872f17f1c24cd3822a4771f5997f9900cb
JJE <elguetaj@ufm.edu>
2020-06-08 16:58:53 -0600

    arregla el bug de cuando las tareas de recepcion no son de erp. Ya no pide confirmacion de recepcion si la tarea no es de erp

--- WmsOnePlan/WMSOnePlan_Client/Classes/WMS_GlobalModule.vb
+++ WmsOnePlan/WMSOnePlan_Client/Classes/WMS_GlobalModule.vb
@@ -70,7 +70,7 @@ Module WMS_GlobalModule
                 If pResult = "OK" Then
 #If DEBUG Then
                     'WSHost = "http://localhost:8088/WMSOnePlan_BusinessServices/"
-                    WSHost = "http://192.168.0.5:9999"
+                    WSHost = "http://172.16.20.11:8088"
 #End If
 #If Not DEBUG Then
                     WSHost = xdata.Tables(0).Rows(0)("WS_HOST").ToString
