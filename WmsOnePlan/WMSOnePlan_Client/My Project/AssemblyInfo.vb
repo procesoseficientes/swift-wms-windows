8ead7cbe69d7def5465c2c0ce279731026461892
JJE <elguetaj@ufm.edu>
2020-07-02 16:03:22 -0600

    Se agrega el campo Tolerancia de expiracion a materiales

--- WmsOnePlan/WMSOnePlan_Client/My Project/AssemblyInfo.vb
+++ WmsOnePlan/WMSOnePlan_Client/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2020.6.16.1")>
-<Assembly: AssemblyFileVersion("2020.6.16.1")>
+<Assembly: AssemblyVersion("2020.7.2.1")>
+<Assembly: AssemblyFileVersion("2020.7.2.1")>
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
