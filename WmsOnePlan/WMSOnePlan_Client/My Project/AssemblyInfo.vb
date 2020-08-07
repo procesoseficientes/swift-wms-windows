ddd6678745a6121868b76ae8c0258259f41b30db
Fabrizzio Rivera <frivera@nevicad.com>
2020-08-07 17:07:12 -0600

    Assembly Icon y cambio cambio en warehouse_id

--- WmsOnePlan/WMSOnePlan_Client/My Project/AssemblyInfo.vb
+++ WmsOnePlan/WMSOnePlan_Client/My Project/AssemblyInfo.vb
@@ -34,8 +34,8 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 
-<Assembly: AssemblyVersion("2020.8.7.0")>
-<Assembly: AssemblyFileVersion("2020.8.7.0")>
+<Assembly: AssemblyVersion("2020.8.7.1")>
+<Assembly: AssemblyFileVersion("2020.8.7.1")>
 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
