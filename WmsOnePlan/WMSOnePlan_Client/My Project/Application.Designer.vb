13a95eadfeca123094d2334406871c3b343d967a
Gildardo Alvarado <gildardo.a@procesoseficientes.com>
2020-12-01 09:25:39 -0600

    Inicio con login

--- WmsOnePlan/WMSOnePlan_Client/My Project/Application.Designer.vb
+++ WmsOnePlan/WMSOnePlan_Client/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WMSOnePlan_Client.FrmLicense
+            Me.MainForm = Global.WMSOnePlan_Client.frmLogin
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
